bdef0019342bba0ba58aca277021bc1c502d58dc
camilo351 <camilo.hernandez.0627@gmail.com>
2024-12-01 18:25:10 -0500

    agregando camilo.py

---
+++ camilo.py
@@ -0,0 +1,7 @@
+def suma ():
+    num1 = int(input("numero 1: "))
+    num2= int(input("numero 2: "))
+    resultado = num1 + num2
+    return resultado
+
+print(f"el resultado de la suma es {suma()}")
