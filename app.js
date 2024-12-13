f3d96a92b90631499e11ed38dc13c3c9a2342416
Camilo Hernandez <camilo.hernandez.0627@gmail.com>
2024-12-13 14:13:04 -0500

    agregando funcion de potencia+

--- app.js
+++ app.js
@@ -8,5 +8,10 @@ function restar(num, num2){
     return resultado
 };
 
+function potencia(num, num2){
+    let resultado = num ** num2 
+    return resultado
+};
+
 document.write(sumar (4, 3));
 document.write(restar (4, 3));
