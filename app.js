cd85d49b292792ccca47a9c9c7af96a187a9bafb
camilo351 <camilo.hernandez.0627@gmail.com>
2024-12-13 14:07:32 -0500

    agregando funciones de multiplicar y dividir

--- app.js
+++ app.js
@@ -8,5 +8,17 @@ function restar(num, num2){
     return resultado
 };
 
+function multiplicar(num,num2){
+    let resultado = num * num2
+    return resultado
+}
+
+function dividir(num,num2){
+    let resultado = num / num2
+    return resultado
+}
+
 document.write(sumar (4, 3));
+document.write(restar (4, 3));
+document.write(restar (4, 3));
 document.write(restar (4, 3));
