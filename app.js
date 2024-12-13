32e4ecf66f42e606638d51b3b8c601240c9ac528
Camilo Hernandez <camilo.hernandez.0627@gmail.com>
2024-12-13 13:55:51 -0500

    Update app.js
    agregando funcion de restar

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 function sumar(num, num2){
     let resultado = num + num2 
     return resultado
-}
+};
+
+function restar(num, num2){
+    let resultado = num - num2 
+    return resultado
+};
 
 document.write(sumar (4, 3));
+document.write(restar (4, 3));
